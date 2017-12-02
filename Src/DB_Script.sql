a79b5a499e17bace2681930410250a69de8adb15
Bruno Carvalho <comedinhass@gmail.com>
2017-12-02 15:54:55 -0200

    Banco atualizado

--- Src/DB_Script.sql
+++ Src/DB_Script.sql
@@ -9,7 +9,7 @@ CREATE TABLE funcionario (
 
 CREATE TABLE comanda(
 	comandaId SERIAL NOT NULL,
-	data date DEFAULT CURRENT_TIMESTAMP,
+	data timestamp DEFAULT CURRENT_TIMESTAMP,
 	mesa varchar(15),
 	valorPago numeric(8,2) DEFAULT 0,
 	status int DEFAULT 0,
@@ -19,10 +19,10 @@ CREATE TABLE comanda(
 CREATE TABLE pagamento (
 	pagamentoId SERIAL PRIMARY KEY NOT NULL,
 	valor int,
-	data date DEFAULT CURRENT_TIMESTAMP,
+	data timestamp DEFAULT CURRENT_TIMESTAMP,
 	funcionarioId int,
 	comandaId int,
-	dataComanda date,
+	dataComanda timestamp,
     foreign key (comandaId, dataComanda) references comanda(comandaId, data) ON DELETE NO ACTION ON UPDATE CASCADE
 );
 
@@ -35,7 +35,7 @@ CREATE TABLE produto (
 
 CREATE TABLE produto_Alterado (
 	produtoId SERIAL REFERENCES produto(produtoId) ON DELETE NO ACTION ON UPDATE CASCADE,
-	data date,
+	data timestamp,
 	valor numeric(8,2),
 	status int DEFAULT 1
 );
@@ -43,7 +43,7 @@ CREATE TABLE produto_Alterado (
 CREATE TABLE produtoComanda (
 	produtoId int REFERENCES produto(produtoId) ON DELETE NO ACTION ON UPDATE CASCADE,
 	comandaId int,
-	dataComanda date,
+	dataComanda timestamp,
 	quantidade int DEFAULT 1,
 	valorPago numeric(8,2) DEFAULT 0,
 	PRIMARY KEY(produtoId, comandaId),
