0321e536776e1b376f4f4a77277c55bddab0f7db
SUdoWinchester <32457585+SUdoWinchester@users.noreply.github.com>
2017-12-04 14:38:53 -0200

    JavaDoc ep. 2

--- Src/src/dao/Comanda.java
+++ Src/src/dao/Comanda.java
@@ -42,7 +42,7 @@ public class Comanda {
 	}
 	
 	/**
-	 * Verifica a existencia de um produto na comanda
+	 * Verifica a existencia de um produto na comanda.
 	 * @param idComanda ID da comanda
 	 * @param data Data da criação da comanda
 	 * @param idProduto ID do Produto
@@ -60,11 +60,11 @@ public class Comanda {
 	}
 	
 	/**
-	 * Atualiza a quantidade de um produto na 
-	 * @param idComanda
-	 * @param data
-	 * @param idProduto
-	 * @param qtde
+	 * Atualiza a quantidade de um produto na comanda.
+	 * @param idComanda ID da comanda
+	 * @param data Data da criação da comanda
+	 * @param idProduto ID do Produto
+	 * @param qtde Nova quantidade de produtos
 	 * @throws Exception
 	 */
 	public static void updateQtde(int idComanda, Timestamp data, int idProduto, int qtde) throws Exception {		
@@ -76,7 +76,15 @@ public class Comanda {
 		ps.setTimestamp(4, data);
 		ps.executeUpdate();
 	}
-
+	
+	/**
+	 * Pega a quantidade de um determinado produto existente em uma comanda 
+	 * @param idComanda ID da comanda
+	 * @param data Data da criação da comanda
+	 * @param idProduto ID do Produto
+	 * @return Retorna a pesquisa com a quantidade de produto
+	 * @throws Exception
+	 */
 	public static ResultSet getQtdeProdutoComanda(int idComanda, Timestamp data, int idProduto) throws Exception {
 		String sql = "SELECT quantidade FROM produtoComanda WHERE produtoId = ? AND comandaId = ? AND comandaData = ?";
 		
@@ -87,6 +95,13 @@ public class Comanda {
 		return ps.executeQuery();
 	}
 	
+	/**
+	 * Remove um produto da comanda
+	 * @param idComanda ID da comanda
+	 * @param data Data da criação da comanda
+	 * @param idProduto ID do Produto
+	 * @throws Exception
+	 */
 	public static void removeProdutoComanda(int idComanda, Timestamp data, int idProduto) throws Exception {
 		String sql = "DELETE FROM produtoComanda WHERE produtoId = ? AND comandaId = ? AND comandaData = ?";
 		
@@ -97,6 +112,14 @@ public class Comanda {
 		ps.executeUpdate();
 	}
 	
+	/**
+	 * Adiciona produto em uma comanda
+	 * @param idComanda ID da comanda
+	 * @param data Data da criação da comanda
+	 * @param idProduto ID do Produto
+	 * @param qtde Quantidade do produto que vai ser adicionado
+	 * @throws Exception
+	 */
 	public static void addProduto(int comandaId, Timestamp data, int produtoId, int qtde) throws Exception {
 		String sql = "INSERT INTO produtoComanda (comandaId, comandaData, produtoId, quantidade) VALUES (?, ?, ?, ?)";
 
@@ -108,7 +131,14 @@ public class Comanda {
 		ps.setInt(4, qtde);
 		ps.executeUpdate();
 	}
-
+	
+	/**
+	 * Pega uma determinada comanda do BD e retorna para o sistema
+	 * @param id ID da comanda
+	 * @param data Data da criação da comanda
+	 * @return Retorna a pesquisa contendo a comanda desejada
+	 * @throws Exception
+	 */
 	public static ResultSet getComanda(int id, Timestamp data) throws Exception {
 		String sql = "SELECT * FROM comanda WHERE comandaId = ? AND data = ?";
 		
@@ -117,7 +147,12 @@ public class Comanda {
 		ps.setTimestamp(2, data);
 		return ps.executeQuery();
 	}
-
+	
+	/**
+	 * Pesquisa e retorna todas a comandas salvas no BD
+	 * @return Retorna a pesquisa contendo as comandas
+	 * @throws Exception
+	 */
 	public static ResultSet getAllComandas() throws Exception {
 		String sql = "SELECT * FROM comanda WHERE pago = ?";
 		
@@ -126,7 +161,14 @@ public class Comanda {
 		
 		return ps.executeQuery();
 	}
-
+	
+	/**
+	 * Pesquisa e retorna todos os produtos de uma determinada comanda
+	 * @param id ID da comanda
+	 * @param data Data da criação da comanda 
+	 * @return Retorna o resultado da pesquisa
+	 * @throws Exception
+	 */
 	public static ResultSet getAllProduto(int id, Timestamp data) throws Exception {		
 		String sql = "SELECT pc.produtoId, p.nome, pc.quantidade, p.valor, SUM(pg.valor) FROM produtoComanda pc LEFT JOIN pagamentoProduto pp ON pp.produtoId = pc.produtoId LEFT JOIN pagamento pg ON pg.pagamentoId = pp.pagamentoId AND pc.comandaId = pg.comandaId AND pc.comandaData = pg.comandaData JOIN produto p ON p.produtoId = pc.produtoId WHERE pc.comandaId = ? AND pc.comandaData = ? GROUP BY 1, 2, 3, 4";
 
@@ -135,7 +177,17 @@ public class Comanda {
 		ps.setTimestamp(2, data);
 		return ps.executeQuery();		
 	}
-
+	
+	/**
+	 * Atualiza alguns campos da comanda.
+	 * @param id ID da comanda
+	 * @param data Data da criação da comanda
+	 * @param mesa Nome da mesa
+	 * @param funcionario ID do funcionario
+	 * @param pago Valor pago
+	 * @return Retorna true caso de certo o update, e flase caso contrario 
+	 * @throws Exception
+	 */
 	public static boolean update(int id, Timestamp data, String mesa, int funcionario, boolean pago) throws Exception {
 		String sql = "UPDATE comanda SET mesa = ?, funcionarioId = ?, pago = ? WHERE comandaId = ? AND data = ?";
 		
@@ -148,7 +200,14 @@ public class Comanda {
 		ps.executeUpdate();
 		return true;
 	}
-
+	
+	/**
+	 * Retorna o valor total da comanda
+	 * @param comandaId ID da comanda
+	 * @param date Data de criação da comanda
+	 * @return Retorna pesquisa
+	 * @throws Exception
+	 */
 	public static float getPrecoComanda(int comandaId, Timestamp date) throws Exception {
 		String sql = "SELECT cp.data, cp.quantidade, p.valor, pa.data, pa.valor FROM comanda c JOIN produtoComanda cp ON c.comandaId = cp.comandaId AND c.data = cp.comandaData JOIN produto p ON p.produtoId = cp.produtoId LEFT JOIN produtoAlterado pa ON p.produtoId = pa.produtoId WHERE c.data = ? AND c.comandaId = ?";
 
@@ -164,7 +223,14 @@ public class Comanda {
 
 		return valor;
 	}
-
+	
+	/**
+	 * Retorna todas as comandas que já foram pagas em determinado período de tempo.
+	 * @param dataDe Data inicial da pesquisa
+	 * @param dataAte Data final da pesquisa
+	 * @return Retorna o resultado da pesquisa
+	 * @throws Exception
+	 */
 	public static ResultSet getAllComandasPagas(Date dataDe, Date dataAte) throws Exception {
 		String sql = "SELECT * FROM comanda WHERE pago = ? AND data >= ? AND data <= ?";
 		
